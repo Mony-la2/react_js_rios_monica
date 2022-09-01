7a9701c471f54a6fcc4b879d4086bd5755e29c39
Monica Rios <monicarios1488@gmail.com>
2022-08-31 22:57:50 -0300

    agregue carrito y texto de bienvenida

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar";
 import './app/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemListContainer from "./components/ItemListContainer";
 
 const App = () => {
 
   return (
     <div>
       <NavBar />
+      <ItemListContainer greetings={'BIENVENIDOS'} />
     </div>
   )
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,10 @@
+
+const ItemListContainer = ({ greetings }) => {
+    return (
+        <h2 className={"textoPrincipal"} >
+            {greetings}
+        </h2>
+    )
+}
+
+export default ItemListContainer;
