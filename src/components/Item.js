2dc2005038c86fa95abff2cb44ed2a3c5c1f00af
Monica Rios <monicarios1488@gmail.com>
2022-09-13 21:11:35 -0300

    correcion item e itemList

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import ItemCount from "./ItemCount";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
 
 
 const Item = ({ item }) => {
@@ -15,7 +18,23 @@ const Item = ({ item }) => {
     }
 
     return (
-        <ItemCount stock="5" initial="1" onAdd={onAdd} />
+        <>
+            <Row className='justify-content-center ms-4 my-4'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/img/${prod.pictureUrl}`} />
+                    <Card.Body>
+                        <Card.Title>{prod.name}</Card.Title>
+                        <Card.Text>
+                            {prod.description}
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroup.Item>${prod.price}</ListGroup.Item>
+                    </ListGroup>
+                    <ItemCount stock="5" initial="1" onAdd={onAdd} />
+                </Card>
+            </Row>
+        </>
     )
 }
 
